afc1735dbf13c1d454c0742bf4cdaada62d7f812
Przemyslaw Wozniak <inz.przemyslaw.wozniak@gmail.com>
2020-02-15 10:24:46 +0100

    Initial commit message

---
+++ src/pl/sda/javawwa30/Computer.java
@@ -0,0 +1,18 @@
+package pl.sda.javawwa30;
+
+public class Computer {
+
+    private String motherboard; // płyta główna
+    private String processor; // "i5", "i7", "intel", "amd"
+    private int cores; // liczba rdzeni
+    private int ram; // ilość ramu
+
+    private int hd; // wielkość dysku w GB
+    private String monitor; // nazwa preducenta
+    private String printer; // nazwa drukarki
+
+    public Computer(String motherboard, String processor, int cores, int ram) {
+
+    }
+
+}
